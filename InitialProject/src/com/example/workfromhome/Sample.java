1f5b00306cc640a559abb9f6cc0eaba22cc4fb55
mohan5246 <63905182+mohan5246@users.noreply.github.com>
2020-08-04 20:12:25 +0530

    Update Sample.java
    Tsts

--- InitialProject/src/com/example/workfromhome/Sample.java
+++ InitialProject/src/com/example/workfromhome/Sample.java
@@ -4,7 +4,7 @@ public class Sample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Initial Program - Try 2");
+		System.out.println("Hello Initial Program - Try 3");
 	}
 
 }
